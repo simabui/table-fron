aff925ef76973c2e3d7a817eed84c9a33fd76b47
simabui <sima_sima@live.ru>
2021-01-07 23:46:26 +0200

    Updates

---
+++ precache-manifest.db24762f8866f944385d8264794173a3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "796523fadc55ed155594",
+    "url": "/table-front/static/js/data.7ab88b99.chunk.js"
+  },
+  {
+    "revision": "25c7350d6bd6d5d46b32",
+    "url": "/table-front/static/css/main.72629705.chunk.css"
+  },
+  {
+    "revision": "25c7350d6bd6d5d46b32",
+    "url": "/table-front/static/js/main.fb4c267e.chunk.js"
+  },
+  {
+    "revision": "5ee173d0216edd365066",
+    "url": "/table-front/static/css/options.d146819f.chunk.css"
+  },
+  {
+    "revision": "5ee173d0216edd365066",
+    "url": "/table-front/static/js/options.1e481f18.chunk.js"
+  },
+  {
+    "revision": "1b3f241562411f9cd6c9",
+    "url": "/table-front/static/js/runtime~main.37369c92.js"
+  },
+  {
+    "revision": "f2dc312b7136bc5a7ad6",
+    "url": "/table-front/static/js/4.ab8f4dc5.chunk.js"
+  },
+  {
+    "revision": "664ece638199a181df0d",
+    "url": "/table-front/static/css/5.f61978d2.chunk.css"
+  },
+  {
+    "revision": "664ece638199a181df0d",
+    "url": "/table-front/static/js/5.e7ae8310.chunk.js"
+  },
+  {
+    "revision": "52fcf51de42fa6ad917460df5b2ea911",
+    "url": "/table-front/index.html"
+  }
+];
